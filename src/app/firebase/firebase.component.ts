9ce0c91ff94ef7926297f27ad376b73c366c6c80
wangbolin <wangbolin@instreet.cn>
2018-11-02 18:27:57 +0800

    uuid ;firebase database

--- src/app/firebase/firebase.component.ts
+++ src/app/firebase/firebase.component.ts
@@ -7,13 +7,15 @@ import { AngularFireMessaging } from '@angular/fire/messaging';
 
 import { AngularFireDatabase } from '@angular/fire/database';
 import { mergeMapTo } from 'rxjs/operators';
+import { v4 as uuid } from 'uuid';
 @Component({
   selector: 'app-firebase',
   templateUrl: './firebase.component.html',
   styleUrls: ['./firebase.component.css']
 })
 export class FirebaseComponent implements OnInit {
-
+  name: string;
+  weight: number;
   // tslint:disable-next-line:max-line-length
   constructor(public afAuth: AngularFireAuth, private storage: AngularFireStorage, private afMessaging: AngularFireMessaging, public db: AngularFireDatabase) {
 
@@ -27,7 +29,7 @@ export class FirebaseComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.listen();
   }
 
   download() {
@@ -43,6 +45,63 @@ export class FirebaseComponent implements OnInit {
 
     this.db.database.ref().once('value', (snapshot) => {
       console.log(snapshot.val());
+
+    });
+
+  }
+
+  adddbt() {
+    const ref = this.db.database.ref('users/').push();
+    ref.set({
+      username: this.name,
+      weight: this.weight
+    }, (e) => {
+      console.log(e, 'complete');
+
+    });
+    // this.db.database.ref('wbl').push({
+
+    //   appid: '666666',
+    //   name: 'flash123',
+    //   version: '1.0.1'
+
+    // }, (e) => {
+    //   console.log(e, 'complete');
+
+    // });
+
+    // this.db.database.ref('test/-LQIWTsjHhZsWntte6DK/version').push({
+    //   appid: '123456',
+    //   name: 'flash',
+    //   version: '1.0.0'
+    // }, (e) => {
+    //   console.log(e, 'complete');
+
+    // });
+  }
+
+  qdbt() {
+    // this.db.database.ref('users').once('value', (shot) => {
+    //   console.log(shot.val());
+
+    // });
+
+    // console.log('new uid: ', uuid());
+
+    // tslint:disable-next-line:max-line-length
+    // this.db.database.ref('users').orderByChild('weight')/* .limitToLast(2) *//* .limitToFirst(2) */.once('value', (snapshot) => {
+    //   console.log(snapshot.val());
+
+    // });
+
+    // this.db.database.ref('users').orderByChild('username').equalTo('wangbolin').once('value', (snap) => {
+    //   console.log(snap.val());
+
+    // });
+
+
+    this.db.database.ref('users').orderByChild('weight').endAt(90)/* .startAt(121) */.once('value', (snap) => {
+      console.log(snap.val());
 
     });
   }
@@ -63,7 +122,7 @@ export class FirebaseComponent implements OnInit {
       .subscribe(
         (token) => { console.log('Permission granted! Save to the server!', token); },
         (error) => { console.error(error); },
-      );
+    );
   }
 
   listen() {
